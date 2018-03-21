23aafeaf3733e0292a39ec0f6b2eb9f414a97997
Andrew Kemp <akemp850@gmail.com>
2018-03-21 17:01:35 +0000

    Added bit docking & tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,7 +4,23 @@ describe DockingStation do
   it 'returns instance of bike class when called release_bike' do
     expect(DockingStation.new.release_bike).to be_a Bike
   end
+
   it 'expect bike to working? or not' do
     expect(Bike.new.working?).to eq true
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
