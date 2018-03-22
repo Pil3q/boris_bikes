06dfd7d47bf836f7487fa895d3b7ee8dd0897515
Patryk Pilecki <ppilecki@icloud.com>
2018-03-22 19:41:09 +0000

    num 15 done

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -6,8 +6,12 @@ class DockingStation
   end
 
   def release_bike
-    raise "there are no bikes" if @bikes == []
+    if empty?
+      raise "there are no bikes"
+    else
     @bikes[0]
+    @bikes.delete_at(0)
+    end
   end
 
   def dock(bike)
@@ -18,9 +22,14 @@ class DockingStation
       @bikes
     end
   end
+  private
 
   def full?
     @bikes.length == 20
+  end
+
+  def empty?
+    @bikes.length == 0
   end
 
 end
